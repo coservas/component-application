f41992c34479d8b33bbcc00c4490a236000bad90
Sergei Vasilev <vasiliev05@gmail.com>
2020-02-07 08:41:53 +0300

    Refactring and add main action.

--- src/Action/NotFoundAction.php
+++ src/Action/NotFoundAction.php
@@ -9,9 +9,6 @@ use Zend\Diactoros\Response\HtmlResponse;
 
 class NotFoundAction implements RequestHandlerInterface
 {
-    /**
-     * @inheritdoc
-     */
     public function handle(ServerRequestInterface $request = null): ResponseInterface
     {
         return new HtmlResponse('Page not found.', 404);
