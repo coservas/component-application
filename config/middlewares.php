450af5dacbe0b408e57bfeef6693e463ebef1b42
Sergei Vasilev <vasiliev05@gmail.com>
2020-02-10 17:45:31 +0300

    Delete middleware for detecting language.

--- config/middlewares.php
+++ config/middlewares.php
@@ -5,7 +5,6 @@ use App\Middleware;
 return [
     Middlewares\ResponseTime::class,
     Middleware\AuthenticationMiddleware::class,
-    Middleware\LanguageMiddleware::class,
     Middlewares\AuraRouter::class,
     Middlewares\RequestHandler::class
 ];
