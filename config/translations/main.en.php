e1a60d2f74ae7d5a40f088ac4332b3e462d13c76
Sergei Vasilev <vasiliev05@gmail.com>
2020-03-01 22:01:37 +0300

    Add base profile templates and translations.

--- config/translations/main.en.php
+++ config/translations/main.en.php
@@ -4,12 +4,19 @@ return [
     'username' => 'Email',
     'password' => 'Password',
     'confirm_password' => 'Confirm password',
+    'fname' => 'Firstname',
+    'sname' => 'Surname',
+    'mname' => 'Middlename',
+    'personal_data' => 'Personal data',
 
     'create_account' => 'Create an account',
     'forgot_password' => 'Forgot password',
     'sign_in' => 'Sign in',
     'register' => 'Register',
     'remember_me' => 'Remember me',
+    'save' => 'Save',
+    'change' => 'Change',
+    'load' => 'Load',
 
     'invalid_username' => 'Invalid email',
     'invalid_password' => 'Invalid password',
@@ -19,4 +26,6 @@ return [
     'service_unavailable' => 'Service unavailable',
     'enter_you_email_and_password' => 'Please enter your user information.',
     'wrong_username_or_password' => 'Wrong username or password',
+
+    'profile' => 'Profile',
 ];
--- config/translations/main.ru.php
+++ config/translations/main.ru.php
@@ -4,12 +4,19 @@ return [
     'username' => 'Электронный адрес',
     'password' => 'Пароль',
     'confirm_password' => 'Повторите пароль',
+    'fname' => 'Имя',
+    'sname' => 'Фамилия',
+    'mname' => 'Отчество',
+    'personal_data' => 'Персональные данные',
 
     'create_account' => 'Создать аккаунт',
     'forgot_password' => 'Восст. пароль',
     'sign_in' => 'Войти',
     'register' => 'Зарегистрироваться',
     'remember_me' => 'Запомнить меня',
+    'save' => 'Сохранить',
+    'change' => 'Изменить',
+    'load' => 'Загрузить',
 
     'invalid_username' => 'Неверный email',
     'invalid_password' => 'Неверный пароль',
@@ -19,4 +26,6 @@ return [
     'service_unavailable' => 'Сервис временно недоступен',
     'enter_you_email_and_password' => 'Введите электронный адрес и пароль',
     'wrong_username_or_password' => 'Неверный электронный адрес или пароль',
+
+    'profile' => 'Профиль',
 ];
